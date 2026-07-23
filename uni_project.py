d966f7ccc3d55d48c2b9cc767392ce7ccd208574
mani <yazdanimani482@gmail.com>
2026-07-23 21:36:59 +0330

    add consturctor with name,family,study,units,student id attributs

--- uni_project.py
+++ uni_project.py
@@ -1,5 +1,13 @@
 class student:
     tuition_per_unit = 12000000
     student_id = 4000
-    
+    def __init__(self,name,family,study,units):
+        student.student_id += 1
+        self.name = name
+        self.family = family
+        self.study = study
+        self.units = units
+        self.student_id_code = student.student_id
+        pass
+
 
