0b25f4ba1895d9bda2b0bc7cf8322a23318c656e
mani <yazdanimani482@gmail.com>
2026-07-24 15:13:20 +0330

    fix methods error and delet all tuition function

--- uni_project.py
+++ uni_project.py
@@ -5,69 +5,66 @@ class student:
 
     def __init__(self,name,family,study,units):
         student.student_id += 1
-        self.name = name
-        self.family = family
-        self.study = study
-        self.units = units
-        self.student_id_code = student.student_id
-        self.tuition = self.all_tuition()
-        self.finaly_tuition = self.discount()
-
-    def all_tuition(self):
-        all_tuition = self.units * student.tuition_per_unit
-        return all_tuition
+        self._name = name
+        self._family = family
+        self._study = study
+        self._units = units
+        self._student_id_code = student.student_id
+        self._finaly_tuition = self.discount()
+         
 
     def discount(self):
+        all_tuition = self._units * student.tuition_per_unit
         if self.units < 12:
-            return self.all_tuition()
+            return all_tuition
         elif 12 <= self.units <= 18:
-            return self.all_tuition() * 0.05
+            return all_tuition * 0.05
         else:
-            return self.all_tuition * 0.1
+            return all_tuition * 0.1
 
     @property
     def name(self):
-        return self.name
+        return self._name
 
     @property
     def family(self):
-        return self.family
+        return self._family
 
     @property
     def study(self):
-        return self.study
+        return self._study
 
     @property
     def units(self):
-        return self.units
+        return self._units
 
     @property
     def studeny_id_code(self):
-        return self.student_id_code
+        return self._student_id_code
 
     @property
     def tuition(self):
-        return self.tuition
+        return self._tuition
 
     @property
-    def discount(self):
-        return self.discount
+    def finaly_tuition(self):
+        return self._finaly_tuition
 
     @name.setter
-    def name(self,value):
-        self.name = value
+    def name(self,name):
+        self._name = name
 
     @family.setter
-    def family(self,value):
-        self.family = value
+    def family(self,family):
+        self._family = family
 
     @study.setter
-    def study(self,value):
-        self.study = value
+    def study(self,study):
+        self._study = study
 
     @units.setter
-    def units(self,value):
-        self.units = value
+    def units(self,units):
+        self._units = units
 
     def __str__(self):
         return f"name and family : {self.name} {self.family}\nstudy:{self.study}\nunits : {self.units}\nstudent code : {self.student_id_code}\ntuition:{self.tuition}"
